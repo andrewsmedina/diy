7979df1e46ee4147bb33cb4f589b119b01133bc1
Andrews Medina <andrewsmedina@gmail.com>
2014-04-22 00:19:54 -0300

    create template list partial.

--- app/js/app.js
+++ app/js/app.js
@@ -4,3 +4,13 @@ var diyApp = angular.module('diyApp', [
 	'ngRoute',
 	'diyControllers'
 ]);
+
+diyApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/templates', {
+		templateUrl: 'partials/template-list.html',
+		controller: 'TemplateListCtrl'
+	}).
+	otherwise({
+		redirectTo: '/templates'
+	});
+}]);
