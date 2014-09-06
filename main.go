0a97b253a7875dae2464ff8667c8d2bb2abd4034
Andrews Medina <andrewsmedina@gmail.com>
2014-09-06 10:34:19 -0300

    serving static files.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -10,7 +11,12 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/static/", staticHandler)
 	http.ListenAndServe(":8080", nil)
 }
